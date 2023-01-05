a6ea2b0345201f23ea24657a524a7b1411f9b3dc
kradijaf <115072007+kradijaf@users.noreply.github.com>
2023-01-05 13:42:51 +0100

    2. verze programu, opraveny issues 1. verze

--- du03_filip_kradijan_seider.py
+++ du03_filip_kradijan_seider.py
@@ -11,34 +11,114 @@ def parse() -> ArgumentParser.parse_args:
     parser.add_argument('-k', action = 'store', nargs ='?', default = 'kontejnery.geojson', dest = 'containers')       # argument -k <názevSouboru> pro soubor s kontejnery
     return parser.parse_args()
 
-def fileControl(addressFile : str, containerFile : str, outputFile : str) -> None:
-    """Kontroluje existenci vstupních souborů, právo s jimi pracovat a správnost adresních bodů."""
+def pointDistance(points1 : tuple, points2 : list) -> float:
+    """Vrací 2D vzdálenost bodů v S-JTSK vypočítanou pomocí Pythagorovy věty zaokrouhlenou na celé metry."""
+    return round(sqrt((points1[0] - points2[0])**2 + (points1[1] - points2[1])**2))
+
+def inputProcessing(addressFile : str, containerFile : str, outputFile : str):
+    """Kontroluje existenci souborů, právo s nimi pracovat, formát a použitelnost dat. Určuje nejbližší kontejner pro 
+    každý adresní bod. Vrací seznamy s nejbližšími vzdálenostmi a adresami, ke kterým se vztahují."""
     try:
-        with open(addressFile, encoding= 'utf-8') as a,\
-        open(containerFile, encoding='utf-8') as c,\
+        with open(addressFile, encoding = 'utf-8') as a,\
+        open(containerFile, encoding ='utf-8') as c,\
         open(outputFile, 'w' ,encoding = 'utf-8') as o:
-            try:
-                aJSON = load(a)
-                cJSON = load(c)
 
-                if len(aJSON['features']) == 0 or len(cJSON['features']) == 0:
-                    print('Vstupní soubor neobsahuje žádné záznamy.')
-                    return False
+            try:        # testování souboru s adresními body
+                aJSON = load(a)     # převod souborů do formátu JSON
 
+                if len(aJSON['features']) == 0:
+                    raise SystemExit('Soubor s adresními body neobsahuje žádné záznamy.')
+            
                 for addressPoint in aJSON['features']:      # kontrola správnosti dat pro každý adresní bod, pokud nejsou všechny adresní body správné, program bude ukočen
                     try:
                         tmp = addressPoint['properties']['addr:street'][0]      # kontrola správnosti ulice
-                        tmp = float(addressPoint['properties']['addr:housenumber'][0])      # kontrola správnosti domovního čísla
-                        for i in addressPoint['geometry']['coordinates']:       # kontrola správnosti souřadnic
-                            tmp = int(i)
-                            if '-' in str(i):
-                                raise ValueError
+                        tmp = [int(i) for i in addressPoint['geometry']['coordinates']]     # kontrola správnosti souřadnic
                     except Exception:
-                        print('Chyba v adresním bodě s identifikátorem: ' + str(addressPoint['properties']['@id']) + '.')
-                        return False
-            except Exception as e:      # jiná chyba ve vstupech
-                print(f'Neočekávaná chyba ve vstupních souborech: {e}.')
-                return False
+                        raise SystemExit('Chyba v adresním bodě s identifikátorem: ' + str(addressPoint['properties']['@id']) + '.')
+            except Exception as e:      # jiná chyba v souboru s adresními body
+                raise SystemExit(f'Neočekávaná chyba v souboru s adresními body: {e}.')
+            
+            try:        # testování souboru s kontejnery
+                cJSON = load(c)
+                if len(cJSON['features']) == 0:
+                    raise SystemExit('Soubor s kontejnery neobsahuje žádné záznamy.')
+            except Exception as e:
+                print(f'Neočekávaná chyba v souboru s kontejnery: {e}.')
+            else:       # výpočet vzdáleností, pokud kontroly proběhly úspěšně
+
+                privateContainers = []      # vytvoření proměnných potřebných pro práci s kontejnery
+                publicContainers = []
+                cFeaturesLen = len(cJSON['features'])
+
+                for container in cJSON['features']:     # rozdělení korektních kontejnerů na volně přístupné a určené obyvatelům dané adresy
+                    try:
+                        tmp = float(container['properties']['ID'])      # kontrola, zda je indentifikátor číslem
+                        tmp = container['properties']['STATIONNAME'][0]     # kontrola správnosti ulice
+                        tmp = [int(i) for i in container['geometry']['coordinates']]        # kontrola správnosti souřadnic
+                    except Exception:
+                        pass
+                    else:       # rozdělení korektních kontejnerů do seznamů pro volně přístupné kontejnery / pro obyvatele domu
+                        containerExtract = {'coordinates' : [float(i) for i in container['geometry']['coordinates']],
+                            'ID' : container['properties']['ID'],
+                            'STATIONNAME' : container['properties']['STATIONNAME']}
+                        if container['properties']['PRISTUP'] == 'obyvatelům domu':
+                            privateContainers.append(containerExtract)
+                        elif container['properties']['PRISTUP'] == 'volně':
+                            publicContainers.append(containerExtract)
+
+                privateContLen = len(privateContainers)
+                publicContLen = len(publicContainers)
+                if (privateContLen + publicContLen) == 0:       # ukončení programu, pokud neexistují kontejnery se správnými daty
+                    raise SystemExit('Neexistuje kontejner s korektními atributy, ukončení programu.')
+
+                nearestDistances = []       # vytvoření proměnných pro výpočet nejbližších kontejnerů 
+                nearestAddresses = []
+                addressPoints = aJSON['features']
+                wgsToSjtsk = Transformer.from_crs(4326, 5514, always_xy= True)
+
+                for addressPoint in addressPoints:      # práce s každým adresním bodem
+                    distance = -1
+                    address = (addressPoint['properties']['addr:street'] + ' ' + str(addressPoint['properties']['addr:housenumber'])).strip()
+
+                    for privateCntnr in privateContainers:      # určení, zda pro adresu existuje soukromý kontejner se stejnou adresou
+                        if privateCntnr['STATIONNAME'].strip() == address:
+                            distance = 0
+                            addressPoint.update({'kontejner' : privateCntnr['ID']})
+                            nearestAddresses.append(address)
+                            nearestDistances.append(distance)
+                            break
+
+                    if distance == 0:
+                        continue
+
+                    # určení nejbližšího veřejného kontejneru, pokud pro adresu neexistuje soukromý
+                    if publicContLen == 0:      # ukončení programu, pokud adrese nelze přiřadit kontejner
+                        ID = addressPoint['properties']['@id']
+                        raise SystemExit(f"""Adresnímu bodu s identifikátorem {ID} nemohl být přiřazen kontejner,\
+vstup neobsahuje veřejně přístupné kontejnery a pro adresu neexistuje soukromý kontejner. Ukončení programu.""")     
+
+                    # vytvoření proměnných pro práci s konkrétní adresou:
+                    SJTSK = wgsToSjtsk.transform(addressPoint['geometry']['coordinates'][0], addressPoint['geometry']['coordinates'][1])       
+                    lowestDistance = -1
+
+                    for publicCntnr in publicContainers:        # určení minimální vzdálenosti pro daný adresní bod
+                        currentDistance = pointDistance(SJTSK, publicCntnr['coordinates'])
+                        if (lowestDistance == -1) or (currentDistance < lowestDistance):
+                            lowestDistance = currentDistance
+                            lowestID = publicCntnr['ID']
+
+                    if lowestDistance > 10000:     # ukončení programu, pokud nejbližší vzdálenost > 10 km
+                        raise SystemExit('Překročena prahová vzdálenost nejbližšího kontejneru 10 km, ukončení programu.')
+                    nearestDistances.append(lowestDistance)        # přidání nejnižší vzdálenosti do seznamu nejnižších vzdáleností
+                    nearestAddresses.append(address)        # přidání adresy adresního bodu do seznamu adres
+                    addressPoint.update({'kontejner' : lowestID})        # přidání identifikátoru nejbližšího kontejneru do záznamu adresního bodu
+
+                with open(outputFile, 'w' ,encoding = 'utf-8') as output:       # uložení adresních bodů s identifikátory nejbližšího kontejnerů do výstupního souboru
+                    dump(addressPoints, output, ensure_ascii = False, indent = 4)
+
+                print(f'Načteno a zpracováno {len(addressPoints)} adresních bodů.')     # výpisy o počtu bodů
+                print(f'Z {cFeaturesLen} načtených kontejnerů bylo zpracováno {len(privateContainers) + len(publicContainers)} korektních kontejnerů.')
+                return nearestDistances, nearestAddresses
     except FileNotFoundError as e:
         print(f'Vstupní soubor neexistuje: {e}.')
     except PermissionError as e:
@@ -47,154 +127,42 @@ def fileControl(addressFile : str, containerFile : str, outputFile : str) -> Non
         print(f'Neočekávaná chyba vyvolaná Vaším počítačem: {e}.')
     except Exception as e:
         print(f'Neočekávaná neznámá chyba: {e}.')
-    else:
-        return True
 
-def wgsToSjtsk(wgsCoords : list) -> tuple:
-    """V n-tici vrací souřadnice převedené z WGS-84 do S-JTSK."""
-    wgs2sjtsk = Transformer.from_crs(4326, 5514, always_xy= True)
-    SJTSK = wgs2sjtsk.transform(wgsCoords[0], wgsCoords[1])
-    return (SJTSK[0], SJTSK[1])
+def findMaximum(distList : list) -> list:
+    """Do seznamu vloží maximum nejbližší vzdálenosti ke kontejneru a její index v seznamu vzdáleností."""
+    maximum = [distList[0], 0]
+    distListLen = len(distList)
+    if distListLen > 1:
+        for i in range(1, distListLen):
+            if distList[i] > maximum[0]:
+                maximum = [distList[i], i]
+    return maximum
 
-def pointDistance(points1 : list, points2 : list) -> float:
-    """Vrací 2D vzdálenost bodů v S-JTSK vypočítanou pomocí Pythagorovy věty zaokrouhlenou na celé metry."""
-    return round(sqrt((points1[0] - points2[0])**2 + (points1[1] - points2[1])**2))
-
-def inputProcessing(addressFile : str, containerFile : str, outputFile : str):
-    """Určuje nejbližší kontejneru pro každý adresní bod. Vrací seznamy s nejbližšími vzdálenostmi a 
-    adresami, ke kterým se vztahují."""
-    with open(addressFile, encoding= 'utf-8') as a,\
-    open(containerFile, encoding='utf-8') as c:
-
-        aJSON = load(a)     # převod souborů do formátu JSON
-        cJSON = load(c)
-        
-        privateContainers = []      # vytvoření proměnných potřebných pro práci s kontejnery
-        publicContainers = []
-        cFeaturesLen = len(cJSON['features'])
-
-        for container in cJSON['features']:     # rozdělení korektních kontejnerů na volně přístupné a určené obyvatelům dané adresy
-            try:
-                tmp = float(container['properties']['ID'])      # kontrola, zda je indentifikátor číslem
-                tmp = container['properties']['STATIONNAME'][0]     # kontrola správnosti ulice
-                for i in container['geometry']['coordinates']:      # kontrola správnosti souřadnic
-                    tmp = int(i)
-                    if '-' not in str(i):
-                        raise ValueError
-            except Exception:
-                pass
-            else:       # rozdělení korektních kontejnerů do seznamů pro volně přístupné kontejnery / pro obyvatele domu
-                if container['properties']['PRISTUP'] == 'obyvatelům domu':
-                    privateContainers.append({'coordinates' : [float(str(i).replace(' ', '')) for i in container['geometry']['coordinates']],
-                                              'ID' : container['properties']['ID'],
-                                              'STATIONNAME' : container['properties']['STATIONNAME']})
-                elif container['properties']['PRISTUP'] == 'volně':
-                    publicContainers.append({'coordinates' : [float(str(i).replace(' ', '')) for i in container['geometry']['coordinates']],
-                                              'ID' : container['properties']['ID'],
-                                              'STATIONNAME' : container['properties']['STATIONNAME']})
-
-        privateContLen = len(privateContainers)
-        publicContLen = len(publicContainers)
-        if (privateContLen + publicContLen) == 0:       # ukončení programu, pokud neexistují kontejnery se správnými daty
-            raise SystemExit('Neexistuje kontejner s korektními atributy, ukončení programu.')
-
-        nearestDistances = []       # vytvoření proměnných pro určení nejbližších kontejnerů 
-        nearestAddresses = []
-        addressPoints = aJSON['features']
-
-        for addressPoint in addressPoints:      # práce s každým adresním bodem
-            distance = -1
-            address = (addressPoint['properties']['addr:street'] + ' ' + str(addressPoint['properties']['addr:housenumber'])).strip()
-
-            for privateCntnr in privateContainers:      # určení, zda pro adresu existuje soukromý kontejner se stejnou adresou
-                if privateCntnr['STATIONNAME'].strip() == address:
-                    distance = 0
-                    addressPoint.update({'kontejner' : privateCntnr['ID']})
-                    nearestAddresses.append(address)
-                    nearestDistances.append(distance)
-                    break
-                
-            if distance == -1:      # určení nejbližšího veřejného kontejneru, pokud pro adresu neexistuje soukromý
-                if publicContLen == 0:      # ukončení programu, pokud adrese nelze přiřadit kontejner
-                    ID = addressPoint['properties']['@id']
-                    raise SystemExit(f'Adresnímu bodu s identifikátorem {ID} nemohl být přiřazen kontejner,\
- vstup neobsahuje veřejně přístupné kontejnery a pro adresu neexistuje soukromý kontejner. Ukončení programu.')     
-
-                # vytvoření proměnných pro práci s konkrétním adresou:
-                SJTSK = wgsToSjtsk([float(str(i).replace(' ', '')) for i in addressPoint['geometry']['coordinates']])       
-                currentDistances = [] 
-                currentIDs = []
-
-                for publicCntnr in publicContainers:        # výpočet vzdálenosti pro každý veřejný kontejner
-                    currentDistances.append(pointDistance(SJTSK, publicCntnr['coordinates']))
-                    currentIDs.append(publicCntnr['ID'])
-
-                minDist = min(currentDistances)     # určení minimální vzdálenosti
-                if minDist > 10000:     # ukončení programu, pokud nejbližší vzdálenost > 10 km
-                    raise SystemExit('Překročena prahová vzdálenost nejbližšího kontejneru 10 km, ukončení programu.')
-                nearestDistances.append(minDist)        # přidání nejnižší vzdálenosti do seznamu nejnižších vzdáleností
-                nearestAddresses.append(address)        # přidání adresy adresního bodu do seznamu adres
-                addressPoint.update({'kontejner' : currentIDs[currentDistances.index(minDist)]})        # přidání identifikátoru nejbližšího kontejneru do záznamu adresního bodu
-
-        with open(outputFile, 'w' ,encoding = 'utf-8') as output:       # uložení adresních bodů s identifikátory nejbližšího kontejnerů do výstupního souboru
-            dump(addressPoints, output, ensure_ascii = False, indent = 4)
-
-        print(f'Načteno a zpracováno {len(addressPoints)} adresních bodů.')     # výpisy o počtu bodů
-        print(f'Z {cFeaturesLen} načtených kontejnerů bylo zpracováno {len(privateContainers) + len(publicContainers)} korektních kontejnerů.')
-        return nearestDistances, nearestAddresses
-
-def strMaximums(maximums : list) -> str:    
-    """Ze seznamu vytvoří řetězec s každou položkou na samostatném řádku."""
-    maximumsStr = f',{",".join(max for max in [*maximums])}'
-    maximumsStr = maximumsStr.replace(',', '\n')
-    return maximumsStr
-
-def findMax(distList : list) -> list:
-    """V závislosti na četnosti hodnoty do seznamu vloží odpovídají počet slovníků obsahující 
-    hodnotu maximální nejbližší vzdálenosti ke kontejneru a její index v seznamu vzdáleností."""
-    max = [{'dist' : distList[0], 'indx' : 0}]
-    for i in range(1, len(distList)):
-        if distList[i] > max[0]['dist']:
-            max = [{'dist' : distList[i], 'indx' : i}]
-        elif distList[i] == max[0]['dist']:
-            max.append({'dist' : distList[i], 'indx' : i}) 
-    return max
-
-def printMax(maxList : list, addrList : list) -> None:
-    """Vypisuje maximum a adresy odpovídající této vzdálenosti k nejbližšímu kontejneru."""
-    maximums = len(maxList)
-    maxDist = maxList[0]['dist']
-    if maximums == 1:
-        indx = maxList[0]['indx']
-        print(f'Maximální vzdálenost k nejbližšímu kontejneru je {maxDist} m a to z adresy {addrList[indx]}.')
-    elif maximums > 1:
-        maxAddresses = [addrList[i['indx']] for i in maxList]
-        maxAddresses = strMaximums(maxAddresses)
-        print(f'Maximální vzdálenost k nejbližšímu kontejneru {maxDist} m byla dosažena pro následující adresy: {maxAddresses}.')
-
-def Median(distList : list, listLen : int) -> None:
+def median(distList : list, listLen : int) -> None:
     """Počítá a vypisuje medián vzdálenosti k nejbližšímu kontejneru."""
     distList.sort()
-    mid = int(listLen / 2)
+    mid = listLen // 2
     if listLen % 2 == 0:        # výpočet v závislosti na sudém / lichém počtu vzdáleností
-        median = round((distList[mid - 1] + distList[mid]) / 2)
+        median = (distList[mid - 1] + distList[mid]) // 2
     else:
         median = distList[mid]
     print(f'Medián průměrné vzdálenosti k nejbližšímu kontejneru je {median} m.')
 
 def statistics(distances : list, addresses : list) -> None:
     """S využitím podfunkcí vypisuje průměrnou vzdálenost a medián vzdálenosti k nejbližšímu 
-    kontejneru a adresu/y, pro které je to nejdál k nejbližšímu kontejneru."""
+    kontejneru a adresu, pro kterou je vzdálenost k nejbližšímu kontejneru nejvyšší."""
     length = len(distances)
     if length == 1:
         print('Vzdálenost k nejbližšímu kontejneru byla vypočtena pouze pro 1 adresu, výsledek nelze s ničím porovnávat.')
-    print(f'Průměrná vzdálenost k nejbližšímu kontejneru je {round(sum(distances) / len(distances))} m.')
+    print(f'Průměrná vzdálenost k nejbližšímu kontejneru je {sum(distances) // len(distances)} m.')
 
-    max = findMax(distances)
-    printMax(max, addresses)
-    Median(distances, length)
+    maximum = findMaximum(distances)
+    print(f'Maximální vzdálenost k nejbližšímu kontejneru je {maximum[0]} m a to z adresy {maximum[1]}.')
+    median(distances, length)
 
 args = parse()
-if fileControl(args.addresses, args.containers, 'adresy_kontejnery.geojson'):
+try:        # prevence pádu pro případ neexistence souboru "adresy.geojson" nebo "kontejnery.geojson" pokud nebyl zadán odpovídající parametr souboru
     dists, addrs = inputProcessing(args.addresses, args.containers, 'adresy_kontejnery.geojson')
     statistics(dists, addrs)
+except TypeError as e:
+    print('Alespoň jeden z výchozích vstupních souborů neexistuje.')
